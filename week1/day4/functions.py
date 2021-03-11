8e21ca48ffe671a68ed05777b914eab68f97b382
Tatien Miller <tatmil99@gmail.com>
2021-03-11 13:06:49 -0600

    practices some concepts

---
+++ week1/day4/functions.py
@@ -0,0 +1,18 @@
+# better way to print add func
+# userInput1 = input("number:")
+# userInput2 = input("number") 
+
+# def add(num1, num2):
+#   return int(num1) + int(num2)  
+
+# print(add(userInput1, userInput2))   
+
+
+# function to print str length
+stringInput = input("type something")
+
+def stringLength(str):
+  lenOfStr = len(str)
+  return lenOfStr
+
+print(stringLength(stringInput))  
---
+++ week1/day4/listTuplesDict.py
@@ -0,0 +1,4 @@
+dictionaryOfUsers = {"name": "Joe", "age": 26, "height": "6-1"}
+
+list = ["item", 2, True]
+
