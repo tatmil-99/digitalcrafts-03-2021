e2445e3b599e8ffca5e2db1b605b01b1e905adef
Tatien Miller <tatmil99@gmail.com>
2021-03-10 18:58:45 -0600

    calculates user input and prints result

---
+++ week1/day3/calculator.py
@@ -0,0 +1,32 @@
+# collects input from user to store values to be calculated
+arg1 = input("First number:")
+arg2 = input("Operator(+, -, *, /):")
+arg3 = input("Second number:")
+
+def add(num1, num2):
+  return int(num1) + int(num2)  
+   
+def subtract(num1, num2):
+  return int(num1) - int(num2)   
+
+def multiply(num1, num2):
+  return int(num1) * int(num2) 
+
+def divide(num1, num2):
+  return int(num1) // int(num2)   
+
+# Depending upon the operator put in by user, the conditions run the correlating function
+# and prints result. 
+if arg2 == "+":
+  print("Result = %s" % (add(arg1, arg3)))    
+elif arg2 == "-":
+  print("Result = %s" % (subtract(arg1, arg3)))    
+elif arg2 == "*":
+  print("Result = %s" % (multiply(arg1, arg3)))    
+elif arg2 == "/":
+  print("Result = %s" % (divide(arg1, arg3))) 
+else:
+  print("Operator not accepted")    
+
+
+
