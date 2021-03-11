223fa24e4f4dc1408d761c2f843580bb55026911
Tatien Miller <tatmil99@gmail.com>
2021-03-11 15:03:09 -0600

    loops lists

---
+++ week1/day4/loopingLists.py
@@ -0,0 +1,26 @@
+lists = ["item", 2, True, "hey"]
+
+# print(len(list)) 
+
+# can loop over a list. while requires a condition
+# index = 0
+# while index < len(lists):
+#   list = lists[index]
+#   print("What item are you? I'm a %s" % list) 
+#   index += 1 
+
+# for loop (for ... in). better to use for lists than while loops 
+# lists index after each item 
+# count = 0
+# for list in lists:
+#   print(list) 
+#   print(count)
+#   count += 1
+
+# combining lists 
+  # use + 
+
+# how do we modify a list?
+lists[0] = "a better item"
+print(lists)
+
