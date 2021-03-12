dcad4c29a0b03bc86be199a36a64f479f730a859
Tatien Miller <tatmil99@gmail.com>
2021-03-11 23:08:24 -0600

    first to-do item

--- week1/day4/toDo.py
+++ week1/day4/toDo.py
@@ -1,18 +1,25 @@
-press_1 = input("press 1 to add task: ")
+print("1. to add task: \n")
+print("2. to delete task: \n")
 
-menu_list = []
-menu_list.append(press_1)
+task1 = input("enter 1 here: ")
+task2 = input("enter 2 here: ")
+task3 = input("enter 3 here: ")
 
-tasks = {}
+menu_list = [task1, task2, task3] 
+ 
+tasks = {} 
+
 for menu in menu_list:
   if menu == menu_list[0]: 
     task_name = input("what is the name of the task? ") 
     priority = input("what is the priority of your task(high, medium, low)? ")
-    tasks = {}
+
     for menu in menu_list:
       tasks["title"] = task_name
       tasks["priority"] = priority 
-
+    
+  elif menu == 2:
+    print(tasks["title"])
 
 print(tasks) 
 
