5018e06d14fc077727c0057fe349a3aa95410dfd
Tatien Miller <tatmil99@gmail.com>
2021-03-12 11:02:33 -0600

    return sum of multiples of 3 and 5 up to 1000

---
+++ week1/day5/multiplesOf3And5.py
@@ -0,0 +1,16 @@
+
+def sumOfMultiples():
+  num = 0
+  total = 0
+  while num < 1000:
+
+    if num % 3 == 0 or num % 5 == 0:
+      total += num
+      num += 1
+    else:
+      num += 1
+  return(total) 
+    
+    
+
+print(sumOfMultiples())
