8d5e7cfbd0dbc6b54d0dfe9641bacd396192e1a9
Tatien Miller <tatmil99@gmail.com>
2021-03-12 23:22:29 -0600

    todo is working

--- week1/day4/toDo.py
+++ week1/day4/toDo.py
@@ -28,6 +28,13 @@ def delTask():
   taskDeleted = tasks.pop(toDelete - 1) 
   print("tasks left: %s" % tasks) 
 
+def viewTask():
+  counter = 1
+  for task in tasks: 
+    toDo = print("%s - %s - %s" %(counter, task["title"], task["priority"]))
+    counter += 1
+  return toDo
+
 choice = "" 
 
 while choice != "q":
@@ -37,9 +44,10 @@ while choice != "q":
     addTask()
   elif keyPressed == "2":
     delTask() 
+  elif keyPressed == "3":
+    viewTask() 
   choice = keyPressed
 
-# print(titlePriority)
 
 
 
@@ -50,29 +58,6 @@ while choice != "q":
 
 
 
-
-
-# task1 = input("enter 1 here: ")
-# task2 = input("enter 2 here: ")
-# task3 = input("enter 3 here: ")
-
-# menu_list = [task1, task2, task3] 
- 
-# tasks = {} 
-
-# for menu in menu_list:
-#   if menu == menu_list[0]: 
-#     task_name = input("what is the name of the task? ") 
-#     priority = input("what is the priority of your task(high, medium, low)? ")
-
-#     for menu in menu_list:
-#       tasks["title"] = task_name
-#       tasks["priority"] = priority 
-    
-#   elif menu == 2:
-#     print(tasks["title"])
-
-# print(tasks) 
 
 
 
