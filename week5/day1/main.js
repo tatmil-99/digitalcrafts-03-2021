9af1034a27bf99887ed4df38557026c720f32c02
Tatien Miller <tatmil99@gmail.com>
2021-04-05 20:26:33 -0500

    weather app is nearly complete

--- week5/day1/main.js
+++ week5/day1/main.js
@@ -15,3 +15,52 @@
 // };
 
 // getWeatherApi();
+
+const api = {
+    key: "369d22c525938e841fa5991e68e8d7d0",
+    url: "https://api.openweathermap.org/data/2.5/"
+};
+
+const searchbox = document.querySelector(".search-box");
+
+searchbox.addEventListener("keydown", event => {
+    if (event.keyCode == 13) {
+        getResults(searchbox.value);
+    }
+});
+
+const getResults = (query) => {
+    fetch(`${api.url}weather?zip=${query}&units=imperial&APPID=${api.key}`)
+        .then(weather => {
+            return weather.json();
+        })  .then(displayResults);
+};
+
+const displayResults = (weather) => {
+    let city = document.querySelector(".location .city");
+    city.innerHTML = `${weather.name} ,${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector(".location .date");
+    date.innerHTML = dateBuilder(now)
+
+    let temp = document.querySelector(".current .temp");
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°F</span>`;
+
+    let weatherElement = document.querySelector(".current .weather");
+    weatherElement.innerHTML = weather.weather[0].main;
+
+    let hiLo = document.querySelector(".hi-lo");
+    hiLo.innerHTML = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)} °F`;
+};
+
+const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day}, ${month} ${date}, ${year}`;
+};
