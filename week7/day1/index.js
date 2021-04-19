8405eb00a88f9dcbfad1133b8496aab5237d1c60
Tatien Miller <tatmil99@gmail.com>
2021-04-19 12:04:05 -0500

    first class server example

--- week7/day1/index.js
+++ week7/day1/index.js
@@ -1,15 +1,38 @@
-console.log("hi");
-
 const http = require("http");
+const express = require("express");
+const app = express();
 const address = "127.0.0.1";
 const port = 3002;
+const {readFile} = require("fs");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-type", "text/plain");
-  res.end("hello");
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  readFile("./index.html", "utf8", (err, html) => {
+    res.send(html);
+  });
+  // res.end("Hello");
 });
 
-server.listen(port, address, () => {
-  console.log(`server is on http://${address}:${port}/`);
+app.get("/home", (req, res) => {
+  readFile("./homepage.html", "utf8", (err, html) => {
+    res.send(html);
+  });
+  // res.end("Hello another place");
 });
+
+app.listen(port, () => {
+  console.log(`Your server is listening on ${port}`);
+});
+
+// not in express
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-type", "text/plain");
+//   res.end("hello");
+// });
+
+// server.listen(port, address, () => {
+//   console.log(`server is on http://${address}:${port}/`);
+// });
