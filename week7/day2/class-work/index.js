732ee4a7dffabc5e00a404d2ddb50812aef6d2de
Tatien Miller <tatmil99@gmail.com>
2021-04-20 15:04:54 -0500

    http gets set up

---
+++ week7/day2/class-work/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+const port = 3001;
+
+const { readFile } = require("fs");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  readFile("./index.html", "utf8", (err, html) => {
+    res.send(html)
+  });
+});
+
+app.get("/about", (req, res) => {
+  const message = "About";
+  res.send(message);
+});
+
+app.get("/contact", (req, res) => {
+  const message = "Contact";
+  res.send(message);
+});
+
+app.post("/stuff", (req, res) => {
+  const message = "Stuff";
+  res.render(message);
+});
+
+app.get("*", (req, res) => {
+  const message = "404";
+  res.send(message);
+});
+
+app.listen(port, () => {
+  console.log(`Your server is listening on ${port}`);
+});
