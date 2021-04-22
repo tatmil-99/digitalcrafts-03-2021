e21f4aa5acfc4f0832bef29f0e834f7d0f53368c
Tatien Miller <tatmil99@gmail.com>
2021-04-21 20:42:01 -0500

    add and read examples

--- week7/day3/index.js
+++ week7/day3/index.js
@@ -8,17 +8,15 @@ const pool = require("./db.js");
 app.use(express.json());
 app.use(cors());
 
-
 app.get("/", (req, res) => {
   res.send("Welcome to node server");
 });
 
 // routes we define for node server
-
 // create todo
 app.post("/todo", async (req, res) => {
   try {
-    const {description} = req.body;
+    const { description } = req.body;
     const newToDoInDB = await pool.query("INSERT INTO todo (description) VALUES($1)", [description]);
     res.json(newToDoInDB);
   } catch (err) {
@@ -26,6 +24,26 @@ app.post("/todo", async (req, res) => {
   }
 });
 
+// read todo list
+app.get("/read_todos", async (req, res) => {
+  try {
+    const readToDoFromDB = await pool.query("SELECT * from todo");
+    res.json(readToDoFromDB);
+  } catch (err) {
+    console.log(err.message);
+  }
+})
+
+// read a specific todo
+app.get("/read_todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const readSingleToDoFromDB = await pool.query("SELECT * from todo WHERE todo_id = ($1)", [id]);
+    res.json(readSingleToDoFromDB);
+  } catch (err) {
+    console.log(err.message);
+  }
+})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
