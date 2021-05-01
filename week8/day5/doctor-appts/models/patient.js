02640e35e8e4207216b7bf4455183bfa96c7a319
Tatien Miller <tatmil99@gmail.com>
2021-04-30 23:01:24 -0500

    patient model created

---
+++ week8/day5/doctor-appts/models/patient.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Patient extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Patient.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Patient',
+  });
+  return Patient;
+};
