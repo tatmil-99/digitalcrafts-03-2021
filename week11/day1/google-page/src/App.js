c009cd00e27c40bfb9c7bdac239f2505c70e528f
Tatien Miller <tatmil99@gmail.com>
2021-05-17 14:44:04 -0500

    five components created

--- week11/day1/google-page/src/App.js
+++ week11/day1/google-page/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
+import Nav from './components/Nav';
 import Main from './components/Main';
-import Nav from './components/Nav'
-import Footer from './components/Footer'
+import Footer from './components/Footer';
 
 function App() {
   return (
--- week11/day1/google-page/src/components/Main.js
+++ week11/day1/google-page/src/components/Main.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
-import Search from './Search'
+import React, { Component } from 'react';
+import Logo from '../Logo';
+import Search from './Search';
 
 export default class Main extends Component {
   render() {
     return (
       <div className="main-container">
+        <Logo/>
         <Search/>
       </div>
     )
