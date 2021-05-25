5ab7176c5ffb6aeb0fa3dee8021ca47721e0bf87
Tatien Miller <tatmil99@gmail.com>
2021-05-24 20:31:55 -0500

    initial commit. Boilerplate removed and components created.

---
+++ week12/day1/movie-app-practice/src/components/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react" 
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Movies</h1>
+    </div>
+  );
+}
+
+export default App;
