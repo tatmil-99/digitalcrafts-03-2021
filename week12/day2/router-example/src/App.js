75b343789f0d99cb02487341a0f29e1a59fb480a
Tatien Miller <tatmil99@gmail.com>
2021-05-25 11:57:42 -0500

    creates router and links components

--- week12/day2/router-example/src/App.js
+++ week12/day2/router-example/src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom"
+import Home from "./components/Home"
+import SocialMedia from "./components/SocialMedia"
+import Portfolio from "./components/Portfolio"
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/socialmedia">Social Media</Link>
+            <Link to="/portfolio">Portfolio</Link>
+          </nav>
+        </div>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/socialmedia">
+          <SocialMedia />
+        </Route>
+        <Route path="/portfolio">
+          <Portfolio />
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Router>
     </div>
   );
 }
--- week12/day2/router-example/src/components/SocialMedia.js
+++ week12/day2/router-example/src/components/SocialMedia.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SocialMedia = () => {
   return (
     <div>
-      
+      <h2>Social Media</h2>
     </div>
   )
 }
