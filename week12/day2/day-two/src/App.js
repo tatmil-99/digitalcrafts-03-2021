01727011b0d686ae33b959198d7f774f03cdd7cb
Tatien Miller <tatmil99@gmail.com>
2021-05-25 11:24:06 -0500

    class example complete

--- week12/day2/day-two/src/App.js
+++ week12/day2/day-two/src/App.js
@@ -1,9 +1,43 @@
 import './App.css';
+import React, { useState } from "react"
+import AboutMe from "./components/AboutMe"
+import Home from "./components/Home"
+import Blog from "./components/Blog"
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom"
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  //potential function to test if a user is logged in
+  const login = () => {
+
+  }
+
   return (
     <div className="App">
-      
+      <Router >
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/blog">Blog</Link>
+          </nav>
+        </div>
+        <Switch>
+          <Route path="/aboutme">
+            <AboutMe />
+          </Route>
+          <Route path="/blog">
+            <Blog />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
