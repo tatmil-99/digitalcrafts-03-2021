f31eb103add8ada9337b6b959b110c25e69b5f54
Tatien Miller <tatmil99@gmail.com>
2021-05-26 13:30:41 -0500

    react counter review

--- week12/day3/redux-day1/src/index.js
+++ week12/day3/redux-day1/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from "redux"
+import { Provider } from "react-redux"
+
+const store = createStore()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
