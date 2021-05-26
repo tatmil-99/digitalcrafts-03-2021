069dd047a65b74e6804172684f89cb6e263072fb
Tatien Miller <tatmil99@gmail.com>
2021-05-26 14:46:57 -0500

    counter made with redux

--- week12/day3/app1/src/components/Count.js
+++ week12/day3/app1/src/components/Count.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { useSelector, useDispatch } from "react-redux"
 
-
 const Count = () => {
+  const count = useSelector((state) => state.count)
   const dispatch = useDispatch();
   return (
     <div>
       <h1>Counter</h1>
-      <p></p>
-      <button>Add</button>
-      <button>Subtract</button>
+      <p>{count}</p>
+      <button onClick={() => dispatch({type: "ADD"})}>Add</button>
+      <button onClick={() => dispatch({type: "SUBTRACT"})}>Subtract</button>
     </div>
   )
 }
