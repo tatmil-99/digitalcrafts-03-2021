d751bf0d50dc14e9026fd10a120ed6f1516bf6e1
Tatien Miller <tatmil99@gmail.com>
2021-05-26 15:42:27 -0500

    changes global state when button is clicked

--- week12/day3/app2/src/index.js
+++ week12/day3/app2/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from "react"
+import { createStore } from "redux"
 import { Provider } from "react-redux"
 import rootReducer from "./reducers/rootReducer"
 
