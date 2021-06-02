06f72505a75464da828b9a21be31714729003c5b
Tatien Miller <tatmil99@gmail.com>
2021-06-01 23:07:42 -0500

    nothing changed

--- week13/day1/controlled-input/src/components/DisplayInput.js
+++ week13/day1/controlled-input/src/components/DisplayInput.js
@@ -1,14 +1,13 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 const DisplayInput = () => {
-  const dispatch = useDispatch();
   const userInfo = useSelector((state) => state.userInfo);
-
+  
   return (
     <div>
       <h1>User Info</h1>
-      <p>{userInfo}</p>
-      <p>{userInfo}</p>
+      <p>{userInfo.username}</p>
+      <p>{userInfo.password}</p>
     </div>
   );
 };
